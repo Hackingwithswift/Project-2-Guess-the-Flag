7eadc7c991ed73a15c0429512d6baf76417adede
Veerababu Mulugu <veeru060786@gmail.com>
2019-06-28 15:38:27 -0600

    Added the initial project

---
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Veerababu Mulugu on 6/28/19.
+//  Copyright © 2019 Veerababu Mulugu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
